bb88ea67e5bf1e37851acf47c7443b35a8e1d328
runner <runner@Mac-1709057190045.local>
2024-02-27 18:14:44 +0000

    Updated to version 1.2.0

--- ios/PluginInfo/_KaleyraVideoHybridVersionInfo.swift
+++ ios/PluginInfo/_KaleyraVideoHybridVersionInfo.swift
@@ -7,7 +7,7 @@ import Foundation
 class _KaleyraVideoHybridVersionInfo: NSObject {
 
     @objc
-    static let krvVersion: String = "1.1.2"
+    static let krvVersion: String = "1.2.0"
 
     @objc
     static let krvPlatform: String = "react"
