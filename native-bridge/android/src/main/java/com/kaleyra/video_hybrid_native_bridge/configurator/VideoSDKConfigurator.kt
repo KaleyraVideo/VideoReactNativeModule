05b4d671d52798bd138df4fe4fbea9847753c469
kristiyan-petrov <kristiyan.petrov@kaleyra.com>
2024-12-05 15:17:48 +0100

    fix: android should use manual notification strategy

--- native-bridge/android/src/main/java/com/kaleyra/video_hybrid_native_bridge/configurator/VideoSDKConfigurator.kt
+++ native-bridge/android/src/main/java/com/kaleyra/video_hybrid_native_bridge/configurator/VideoSDKConfigurator.kt
@@ -6,6 +6,7 @@ package com.kaleyra.video_hybrid_native_bridge.configurator
 import com.kaleyra.video_common_ui.CallUI
 import com.kaleyra.video_common_ui.ChatUI
 import com.kaleyra.video_common_ui.KaleyraVideo
+import com.kaleyra.video_common_ui.PushNotificationHandlingStrategy
 import com.kaleyra.video_hybrid_native_bridge.KaleyraVideoConfiguration
 import com.kaleyra.video_hybrid_native_bridge.extensions.toCallActions
 import com.kaleyra.video_hybrid_native_bridge.extensions.toChatActions
@@ -16,37 +17,38 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 internal class VideoSDKConfigurator(
-    private val sdk: KaleyraVideo,
-    private val repository: VideoHybridBridgeRepository,
-    private val scope: CoroutineScope
+  private val sdk: KaleyraVideo,
+  private val repository: VideoHybridBridgeRepository,
+  private val scope: CoroutineScope
 ) : CachedSDKConfigurator {
 
-    override var lastConfiguration: KaleyraVideoConfiguration? = null
+  override var lastConfiguration: KaleyraVideoConfiguration? = null
 
-    override fun configureBridge(configuration: KaleyraVideoConfiguration) {
-        scope.launch {
-            repository.configurationDao().insert(ConfigurationEntity(configuration))
-            sdk.configure(configuration.toSDK())
-            sdk.conference.callActions = configuration.tools?.toCallActions() ?: CallUI.Action.default
-            sdk.conversation.chatActions = configuration.tools?.toChatActions() ?: ChatUI.Action.default
-            lastConfiguration = configuration
-        }
+  override fun configureBridge(configuration: KaleyraVideoConfiguration) {
+    scope.launch {
+      repository.configurationDao().insert(ConfigurationEntity(configuration))
+      sdk.configure(configuration.toSDK())
+      sdk.conference.callActions = configuration.tools?.toCallActions() ?: CallUI.Action.default
+      sdk.conversation.chatActions = configuration.tools?.toChatActions() ?: ChatUI.Action.default
+      lastConfiguration = configuration
+      sdk.pushNotificationHandlingStrategy = PushNotificationHandlingStrategy.Manual
     }
+  }
 
-    override fun reset() {
-        scope.launch {
-            repository.clearAllTables()
-            sdk.reset()
-            KaleyraVideo.conference.callActions = CallUI.Action.default
-            KaleyraVideo.conversation.chatActions = ChatUI.Action.default
-            lastConfiguration = null
-        }
+  override fun reset() {
+    scope.launch {
+      repository.clearAllTables()
+      sdk.reset()
+      KaleyraVideo.conference.callActions = CallUI.Action.default
+      KaleyraVideo.conversation.chatActions = ChatUI.Action.default
+      lastConfiguration = null
     }
+  }
 
-    init {
-        scope.launch {
-            lastConfiguration = repository.configurationDao().configuration?.plugin
-        }
+  init {
+    scope.launch {
+      lastConfiguration = repository.configurationDao().configuration?.plugin
     }
+  }
 
 }
