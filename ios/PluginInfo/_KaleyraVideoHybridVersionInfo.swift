d262848edf017726f35661eb55329fdc013e6854
Anka <runner@Mac-1735558464868.local>
2024-12-30 11:52:16 +0000

    Updated to version 2.0.3

--- ios/PluginInfo/_KaleyraVideoHybridVersionInfo.swift
+++ ios/PluginInfo/_KaleyraVideoHybridVersionInfo.swift
@@ -7,7 +7,7 @@ import Foundation
 class _KaleyraVideoHybridVersionInfo: NSObject {
 
     @objc
-    static let krvVersion: String = "2.0.2"
+    static let krvVersion: String = "2.0.3"
 
     @objc
     static let krvPlatform: String = "react"
