dccb3b3056e98cd41adf0fc5a246c36b5e4b464a
Federico Marin <137904192+fede987@users.noreply.github.com>
2026-01-07 11:56:04 +0100

    chore: simplify Babel configuration by removing test env

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    test: {
-      presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
-      ],
-    },
-    default: {
-      presets: ['module:metro-react-native-babel-preset'],
-    },
-  },
+  presets: ['module:metro-react-native-babel-preset'],
 };
